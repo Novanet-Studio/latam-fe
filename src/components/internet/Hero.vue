eb2e7812ecc04bfdff9af39f7dec363ea8715ce8
Freddy Polania <fpolania@gmail.com>
2021-05-06 15:48:24 -0400

    Ajustes vista internet

--- src/components/internet/Hero.vue
+++ src/components/internet/Hero.vue
@@ -1,15 +1,13 @@
 <template>
   <section class="hero">
-    <div class="hero__vacio"></div>
-
-    <div class="hero__internet">
-      <div class="hero__internet__box">
-        <div class="hero__internet__icon"></div>
-        <div class="hero__internet__info">
-          <h1 class="hero__internet__title">
+    <div class="hero__internet-vista">
+      <div class="hero__internet-vista__box">
+        <div class="hero__internet-vista__icon"></div>
+        <div class="hero__internet-vista__info">
+          <h1 class="hero__internet-vista__title">
             Internet corporativo de alta velocidad
           </h1>
-          <p class="hero__internet__text">
+          <p class="hero__internet-vista__text">
             Conexión en fibra óptica dirigida a uso corporativo y residencial de
             alta gama.
           </p>
