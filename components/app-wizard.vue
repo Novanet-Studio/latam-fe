7b82a491189b69ae46ba5a40848ae6c3c68ef0ae
David Arenas <davejs136@gmail.com>
2024-01-22 14:48:50 -0400

    fmt: format code

--- components/app-wizard.vue
+++ components/app-wizard.vue
@@ -1,82 +1,98 @@
 <template>
   <section class="wizard-light">
-    <app-stepper :stepper="stepper" :check-disabled="allStepsBeforeAreValid"  />
+    <app-stepper :stepper="stepper" :check-disabled="allStepsBeforeAreValid" />
     <div class="wizard payment-section">
       <h3>Confirmación de Pagos TV por cable</h3>
       <h5>{{ stepper.current.value.title }}</h5>
 
       <form @submit.prevent="submit">
-        <transition :name="isNextClicked ? 'slide-left' : 'slide-right'" mode="out-in">
-          <app-loader v-if="form.status === 'pending'" class="loader-wrapper__wizard" />
+        <transition
+          :name="isNextClicked ? 'slide-left' : 'slide-right'"
+          mode="out-in"
+        >
+          <app-loader
+            v-if="form.status === 'pending'"
+            class="loader-wrapper__wizard"
+          />
           <component v-else :is="activeComponent" />
         </transition>
       </form>
 
       <div class="wizard__footer">
-        <button v-if="!stepper?.isFirst.value" class="wizard__btn wizard__btn--prev" @click="() => {
-          isNextClicked = false
-          stepper.goToPrevious()
-        }">Regresar</button>
-        <button class="wizard__btn wizard__btn--next" @click="() => {
-          isNextClicked = true
-          submit()
-        }">{{ nextBtnLabel }}</button>
+        <button
+          v-if="!stepper?.isFirst.value"
+          class="wizard__btn wizard__btn--prev"
+          @click="
+            () => {
+              isNextClicked = false;
+              stepper.goToPrevious();
+            }
+          "
+        >
+          Regresar
+        </button>
+        <button
+          class="wizard__btn wizard__btn--next"
+          @click="
+            () => {
+              isNextClicked = true;
+              submit();
+            }
+          "
+        >
+          {{ nextBtnLabel }}
+        </button>
       </div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import { useStepper } from '@vueuse/core'
+import { useStepper } from "@vueuse/core";
 
-import CheckSubscriptionStep from './check-subscription-step.vue';
-import SubscriptorDataStep from './subscriptor-data-step.vue';
-import PaymentReportStep from './payment-report-step.vue';
-import StatusStep from './status-step.vue';
-import AppStepper from './app-stepper.vue';
+import CheckSubscriptionStep from "./check-subscription-step.vue";
+import SubscriptorDataStep from "./subscriptor-data-step.vue";
+import PaymentReportStep from "./payment-report-step.vue";
+import StatusStep from "./status-step.vue";
+import AppStepper from "./app-stepper.vue";
 
 const isNextClicked = ref(false);
-const form = inject('form') as Latam.Form;
+const form = inject("form") as Latam.Form;
+
+const { btBaseApi, bussiness } = useRuntimeConfig().public;
 
 const stepper = useStepper({
-  'check-subscription': {
-    title: 'Consultar datos de suscriptor',
+  "check-subscription": {
+    title: "Consultar datos de suscriptor",
     isValid: () => form.contract.length > 0,
   },
-  'subscriptor-data': {
-    title: 'Datos del subscriptor',
+  "subscriptor-data": {
+    title: "Datos del subscriptor",
     isValid: () => true,
   },
-  'payment-report': {
-    title: 'Reporte de pago',
-    isValid: () => form.phone.length > 0 && form.ci.length > 0 && form.bank.length > 0 && form.paymentDate.length > 0 && form.paymentReference.length > 0,
+  "payment-report": {
+    title: "Reporte de pago",
+    isValid: () =>
+      form.phone.length > 0 &&
+      form.ci.length > 0 &&
+      form.bank.length > 0 &&
+      form.paymentDate.length > 0 &&
+      form.paymentReference.length > 0,
   },
-  'status': {
-    title: 'Estatus de pago',
-    isValid: () => true
+  status: {
+    title: "Estatus de pago",
+    isValid: () => true,
   },
 });
 
-function submit() {
-  if (stepper.current.value.isValid())
-    stepper.goToNext()
-}
-
-function allStepsBeforeAreValid(index: number): boolean {
-  return !Array(index)
-    .fill(null)
-    .some((_, i) => !stepper.at(i)?.isValid())
-}
-
-
 const activeComponent = computed(() => {
-  if (stepper.isCurrent('check-subscription')) {
+  if (stepper.isCurrent("check-subscription")) {
     return CheckSubscriptionStep;
-  } else if (stepper.isCurrent('subscriptor-data')) {
+  } else if (stepper.isCurrent("subscriptor-data")) {
     return SubscriptorDataStep;
-  } else if (stepper.isCurrent('payment-report')) {
+  } else if (stepper.isCurrent("payment-report")) {
     return PaymentReportStep;
-  } else if (stepper.isCurrent('status')) {
+  } else if (stepper.isCurrent("status")) {
     return StatusStep;
   }
 });
@@ -84,12 +100,102 @@ const activeComponent = computed(() => {
 const nextBtnLabel = computed(() => {
   if (stepper.isLast.value) {
     return "Regresar al inicio";
-  } else if (stepper.isCurrent('payment-report')) {
+  } else if (stepper.isCurrent("payment-report")) {
     return "Finalizar";
   } else {
     return "Siguiente";
   }
-})
+});
+
+interface KeyRequest {
+  codResp: string;
+  descResp: string;
+  claveDinamica: string;
+}
+
+interface PaymentResponse {
+  codres: string;
+  descResp: string;
+  autorizacionISO: string;
+  traceISO: string;
+  autorizacionIBSMonto: string;
+  autorizacionIBSComision: string;
+  montoComision: string;
+  referencia: string;
+  fecha: string; // DD/MM/YYYY
+  hora: string; // HH:MM:SS
+  claveDinamica: number;
+  monto: string;
+  numeroLote: string;
+}
+
+async function submit() {
+  if (stepper.isCurrent("payment-report")) {
+    try {
+      form.status = "pending";
+      // Peticion para el token
+      const { data: token } = await useFetch<KeyRequest>(
+        `${btBaseApi}/lotes/solicitud/clave`,
+        {
+          method: "POST",
+          body: {
+            canal: "01",
+            celularDestino: "V011484286",
+          },
+        }
+      );
+
+      if (!token.value?.claveDinamica) {
+        // TODO: show error or some modal
+        alert(token.value?.descResp);
+        return;
+      }
+
+      // Procesar pago
+      const { data: payment } = await useFetch<PaymentResponse>(
+        `${btBaseApi}/botonDePago/pago`,
+        {
+          method: "POST",
+          body: {
+            canal: "06",
+            celular: "04122084674",
+            banco: "0163",
+            RIF: "J297059172",
+            cedula: "V011484286",
+            monto: "3617,00",
+            token: "14866991",
+            concepto: "prueba",
+            codAfiliado: "004036",
+            comercio: "Latam",
+          },
+        }
+      );
+
+      console.log("token =>", token.value?.claveDinamica);
+      console.log("payment =>", payment.value);
+
+      // Conformacion de pago
+
+      // mostrar mensaje de exito o error
+      stepper.goToNext();
+    } catch (error) {
+      form.status = "error";
+      // TODO: show error or some modal
+    } finally {
+      form.status = "success";
+    }
+    
+    return
+  }
+
+  if (stepper.current.value.isValid()) stepper.goToNext();
+}
+
+function allStepsBeforeAreValid(index: number): boolean {
+  return !Array(index)
+    .fill(null)
+    .some((_, i) => !stepper.at(i)?.isValid());
+}
 
 provide("stepper", stepper);
 </script>
@@ -133,7 +239,6 @@ provide("stepper", stepper);
     margin-bottom: 2rem;
     width: 100%;
 
-
     & div label {
       color: #666666;
       font-weight: 500;
@@ -159,12 +264,12 @@ provide("stepper", stepper);
   }
 
   & .wizard__btn--next {
-    background: #C2D62E;
-    color: #1B4686;
+    background: #c2d62e;
+    color: #1b4686;
   }
 
   & .wizard__btn--prev {
-    background-color: #1B4686;
+    background-color: #1b4686;
     color: #fff;
   }
 }
