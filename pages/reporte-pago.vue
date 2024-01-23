6fb88bedaaf747bb2405e417f4d479d7fec6470f
David Arenas <davejs136@gmail.com>
2024-01-22 22:04:09 -0400

    refactor: add `payment-report` page

--- pages/reporte-pago.vue
+++ pages/reporte-pago.vue
@@ -16,9 +16,47 @@
         </div>
       </div>
     </section>
+
+    <section class="payment">
+      <h2 class="payment__title">Información de pago</h2>
+      <p>
+        Una vez realizado el pago es obligatorio hacer la notificacion del
+        mismo, utilizando uno de los dos medios de notificación, página web o
+        whatsapp al 0414 3021751. Utilice un solo método de notificación, solo
+        la página web o el whatsapp. ( INGRESAR POR EL NAVEGADO MOZILLA FIREFOX
+        PARA REGISTRAR SU PAGO )
+      </p>
+
+      <div class="payment__info">
+        <div class="info-box">
+          <div class="info-box__icon-wrapper">
+            <img src="../assets/images/latinamerican-transferencia-icon.svg" alt="" />
+          </div>
+          <div>
+            <h3 class="info-box__title">Transferencia bancaria</h3>
+            <p>
+              Cuenta a la cual cual debe realizar la transferencia: <br />
+              Cuenta corriente: 0172 0104 2910 4436 9427 <br />
+              Banco: Bancamiga. <br />
+              RIF: J-298946229 <br />
+            </p>
+          </div>
+        </div>
+        <div class="info-box">
+          <div class="info-box__icon-wrapper">
+            <img src="../assets/images/latinamerican-transferencia-icon.svg" alt="" />
+          </div>
+          <div>
+            <h3 class="info-box__title">Pago móvil</h3>
+            <p>
+              Número teléfonico al cual debe realizar el pago: <br />
+              0414.203.2323 <br />
+              Banco: Banesco. <br />
+              RIF: J-298946229 <br />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
-
-<script lang="ts" setup>
-navigateTo("http://190.216.243.210:8822/", { external: true, replace: true });
-</script>
