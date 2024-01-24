91655ffcd1a797155b1ac27faf74d09b66d06873
David Arenas <davejs136@gmail.com>
2024-01-24 14:56:22 -0400

    add: return example object

--- server/api/get-client-details.post.ts
+++ server/api/get-client-details.post.ts
@@ -4,26 +4,30 @@ interface Body {
 }
 
 export default defineEventHandler(async (event) => {
-  const { usersApi, usersApiKey } = useRuntimeConfig(event);
 
-  try {
-    const data = (await readBody(event)) as Body;
-
-    const response = await fetch(`${usersApi}/GetClientsDetails`, {
-      method: 'POST',
-      body: JSON.stringify({
-        token: usersApiKey,
-        cedula: data.cedula
-      })
-    });
-
-    const json = await response.json();
-
-    return json;
-  } catch (error: any) {
-    return createError({
-      statusCode: 500,
-      message: "Error Interno del Servidor, intente otra vez",
-    })
+  return {
+    message: "hola"
   }
+  // const { usersApi, usersApiKey } = useRuntimeConfig(event);
+
+  // try {
+  //   const data = (await readBody(event)) as Body;
+
+  //   const response = await fetch(`${usersApi}/GetClientsDetails`, {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       token: usersApiKey,
+  //       cedula: data.cedula
+  //     })
+  //   });
+
+  //   const json = await response.json();
+
+  //   return json;
+  // } catch (error: any) {
+  //   return createError({
+  //     statusCode: 500,
+  //     message: "Error Interno del Servidor, intente otra vez",
+  //   })
+  // }
 })
