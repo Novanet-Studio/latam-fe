18ca27ad01acc9bc2953a49dc277445decc87047
David Arenas <davejs136@gmail.com>
2024-01-24 23:37:15 -0400

    refactor(nuxt): remove preset & commented code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,20 +34,12 @@ export default defineNuxtConfig({
   },
   modules: ["@nuxt/image", "@nuxtjs/strapi", "@kevinmarrec/nuxt-pwa", "@vueuse/nuxt"],
   runtimeConfig: {
-    // btBaseApi: process.env.BT_BASE_API_URL,
-    // usersApi: process.env.USERS_API_URL,
-    // usersApiKey: process.env.USERS_API_KEY,
     public: {
       strapi: {
         url: process.env.STRAPI_URL || "http://localhost:1337",
       },
       bcvUsdApi: process.env.BCV_USD_API_URL,
       latamServicesApiUrl: process.env.LATAM_SERVICES_API
-      // bussiness: {
-      //   rif: process.env.BUSSINESS_RIF,
-      //   name: process.env.BUSSINESS_NAME,
-      //   afiliatedCode: process.env.AFILIATED_CODE,
-      // }
     },
   },
   typescript: {
@@ -77,8 +69,4 @@ export default defineNuxtConfig({
       "2xl": 1536,
     },
   },
-
-  nitro: {
-    preset: 'netlify-builder'
-  }
 });
