920b84526a5da17cde930aed57da67b1aa874898
David Arenas <davejs136@gmail.com>
2024-02-23 10:00:40 -0400

    fix: isAuthenticated flag

--- pages/reporte-pago.vue
+++ pages/reporte-pago.vue
@@ -11,7 +11,7 @@ useHead({
   ],
 });
 
-const isAuthenticated = ref(true);
+const isAuthenticated = ref(false);
 const isLoading = ref(false)
 const userData = reactive<Latam.UserData>({
   estado: '',
