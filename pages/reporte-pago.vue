022ac0d47b1a88b0c1e4eaadffc178ac603c9273
Fredy Polania <fpolania@gmail.com>
2024-03-11 13:16:20 -0400

    remove: New payment information

--- pages/reporte-pago.vue
+++ pages/reporte-pago.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import AppLogin from '~/components/app-login.vue';
-import AppWizard from '~/components/app-wizard.vue'; 
+import AppLogin from "~/components/app-login.vue";
+import AppWizard from "~/components/app-wizard.vue";
 
 useHead({
   meta: [
@@ -12,31 +12,33 @@ useHead({
 });
 
 const isAuthenticated = ref(false);
-const isLoading = ref(false)
+const isLoading = ref(false);
 const userData = reactive<Latam.UserData>({
-  estado: '',
+  estado: "",
   datos: null,
 });
 
 const form = reactive<Latam.Form>({
-  ci: '',
-  contract: '',
-  fullName: '',
+  ci: "",
+  contract: "",
+  fullName: "",
   amount: 0,
-  vesAmount: '',
-  phone: '',
-  bank: '',
-  paymentDate: '',
-  dynamicKey: '',
-  status: '' // error | pending | success
+  vesAmount: "",
+  phone: "",
+  bank: "",
+  paymentDate: "",
+  dynamicKey: "",
+  status: "", // error | pending | success
 });
 
-const activeComponent = computed(() => isAuthenticated.value ? AppWizard : AppLogin);
+const activeComponent = computed(() =>
+  isAuthenticated.value ? AppWizard : AppLogin
+);
 
-provide('form', form);
-provide('userData', userData);
-provide('isLoading', isLoading);
-provide('isAuthenticated', isAuthenticated);
+provide("form", form);
+provide("userData", userData);
+provide("isLoading", isLoading);
+provide("isAuthenticated", isAuthenticated);
 </script>
 
 <template>
@@ -46,11 +48,10 @@ provide('isAuthenticated', isAuthenticated);
         <div class="hero__internet__box">
           <div class="hero__internet__icon"></div>
           <div class="hero__internet__info">
-            <h1 class="hero__internet__title">
-              Reporta tus pagos
-            </h1>
+            <h1 class="hero__internet__title">Realiza tus pagos</h1>
             <p class="hero__internet__text">
-              Ponemos a tu disposición formas para que notifiques los pagos que realices
+              Ponemos a tu disposición el botón de pago del Banco del Tesoro
+              para que puedas pagar de forma rápida y segura
             </p>
           </div>
         </div>
@@ -58,45 +59,13 @@ provide('isAuthenticated', isAuthenticated);
     </section>
 
     <section class="payment">
-      <h2 class="payment__title">Información de pago</h2>
+      <h2 class="payment__title">Haz tus pagos de forma rápida y segura</h2>
       <p>
-        Una vez realizado el pago es obligatorio hacer la notificacion del
-        mismo, utilizando uno de los dos medios de notificación, página web o
-        whatsapp al 0414 3021751. Utilice un solo método de notificación, solo
-        la página web o el whatsapp. ( INGRESAR POR EL NAVEGADO MOZILLA FIREFOX
-        PARA REGISTRAR SU PAGO )
+        Sigue las indicaciones del formulario multipasos mostrado a
+        continuación, las datos de acceso son los mismos que utilizas en el
+        portal Acceso a Clientes. Una vez realizado el pago es obligatorio hacer
+        la notificacion del mismo por WhatApp al 0414 3021751.
       </p>
-
-      <div class="payment__info">
-        <div class="info-box">
-          <div class="info-box__icon-wrapper">
-            <img src="../assets/images/latinamerican-transferencia-icon.svg" alt="" />
-          </div>
-          <div>
-            <h3 class="info-box__title">Transferencia bancaria</h3>
-            <p>
-              Cuenta a la cual cual debe realizar la transferencia: <br />
-              Cuenta corriente: 0172 0104 2910 4436 9427 <br />
-              Banco: Bancamiga. <br />
-              RIF: J-298946229 <br />
-            </p>
-          </div>
-        </div>
-        <div class="info-box">
-          <div class="info-box__icon-wrapper">
-            <img src="../assets/images/latinamerican-transferencia-icon.svg" alt="" />
-          </div>
-          <div>
-            <h3 class="info-box__title">Pago móvil</h3>
-            <p>
-              Número teléfonico al cual debe realizar el pago: <br />
-              0414.203.2323 <br />
-              Banco: Banesco. <br />
-              RIF: J-298946229 <br />
-            </p>
-          </div>
-        </div>
-      </div>
     </section>
 
     <section class="report">
