41da3c2b7355de0883af7cc103a101243bf3ee7c
David Arenas <davejs136@gmail.com>
2024-07-23 14:32:56 -0400

    refactor: change provider id

--- composables/use-payments.ts
+++ composables/use-payments.ts
@@ -150,7 +150,7 @@ export default function usePayments({
               Id: {
                 PrvtId: {
                   Othr: {
-                    Id: "800001",
+                    Id: "0071",
                   },
                 },
               },
