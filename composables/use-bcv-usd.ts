f48232dafe73c41fea8a9e804c99700b89213f78
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-01-07 09:46:02 -0400

    fix: change USD-VES convertion API

--- composables/use-bcv-usd.ts
+++ composables/use-bcv-usd.ts
@@ -1,23 +1,22 @@
 interface BcvUsdResponse {
-  name: string;
-  pair: string;
-  sources: {
-    BCV: {
-      name: string;
-      quote: string;
-    };
-  };
+  fuente: string;
+  nombre: string;
+  compra?: any;
+  venta?: any;
+  promedio: number;
+  fechaActualizacion: string;
 }
 
 export default function useBcvUsd() {
   const { bcvUsdApi } = useRuntimeConfig().public;
-  const usd = ref('');
+  const usd = ref("");
 
   onMounted(async () => {
     try {
       const response = await fetch(bcvUsdApi);
       const result: BcvUsdResponse = await response.json();
-      const quote = transformAmount(result.sources.BCV.quote)
+      const quote = transformAmount(String(result.promedio));
+
       usd.value = quote;
     } catch (err) {
       console.log(err);
